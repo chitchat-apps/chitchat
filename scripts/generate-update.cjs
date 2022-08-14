e7c996978bdc291139a24b60cc59bdf82d3ac55f
Sebastian Vallin <sebbe.vallin@gmail.com>
2022-08-14 22:41:09 +0200

    fix script

--- scripts/generate-update.cjs
+++ scripts/generate-update.cjs
@@ -1,11 +1,8 @@
 const fs = require("fs");
-const path = require("path");
 
 const version = require("../package.json").version;
 
 const filePath = "src-tauri/target/release.json";
-
-const signature = fs.readFileSync("chitchat.key.pub", "utf8");
 
 const json = {
   version: version,
@@ -13,16 +10,16 @@ const json = {
   pub_date: new Date(),
   platforms: {
     "darwin-x86_64": {
-      signature,
+      signature: "SIGNATURE_HERE",
       url: "https://github.com/chitchat-apps/chitchat/releases/latest/download/ChitChat-Mac-x86_64.app.tar.gz",
     },
     "darwin-aarch64": {
-      signature,
+      signature: "SIGNATURE_HERE",
       url: "https://github.com/chitchat-apps/chitchat/releases/latest/download/ChitChat-Mac-aarch64.app.tar.gz",
     },
     "windows-x86_64": {
-      signature,
-      url: "https://github.com/chitchat-apps/chitchat/releases/latest/download/https://github.com/chitchat-apps/chitchat/releases/latest/download/ChitChat-Setup-Windows.msi.zip",
+      signature: "SIGNATURE_HERE",
+      url: "https://github.com/chitchat-apps/chitchat/releases/latest/download/ChitChat-Setup-Windows.msi.zip",
     },
   },
 };
