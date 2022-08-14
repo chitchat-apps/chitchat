1b2f89e32919415c39294c924a14ac3aa5914602
Sebastian Vallin <sebbe.vallin@gmail.com>
2022-08-14 22:50:21 +0200

    fix: version in settings page

--- src/pages/Settings/AboutPage.tsx
+++ src/pages/Settings/AboutPage.tsx
@@ -1,12 +1,15 @@
-import { Box, Heading, Text, VStack } from "@chakra-ui/react";
+import { Heading, Text, VStack } from "@chakra-ui/react";
 import { FC } from "react";
+import { getVersion } from "@tauri-apps/api/app";
+
+const version = await getVersion();
 
 const AboutPage: FC = () => {
   return (
     <VStack w="100%" h="100%" align="center" justify="center" spacing={0}>
       <VStack spacing={0} align="start">
         <Heading textAlign="left">ChitChat</Heading>
-        <Text textAlign="left">Version 0.0.1</Text>
+        <Text textAlign="left">Version {version}</Text>
       </VStack>
     </VStack>
   );
