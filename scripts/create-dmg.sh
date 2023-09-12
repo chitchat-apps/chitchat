dfdd29461c7acd7e95ed8fd9d471cf4f34dc20fc
Sebastian Vallin <sebbe.vallin@gmail.com>
2023-09-12 12:03:03 +0200

    Add script to create DMG installer for ChitChat v0.1.0
    This commit adds a new script, "create-dmg.sh", which creates a DMG installer for the ChitChat application. The script sets up variables for the app version, name, and file paths. It then copies the chitchat.app to the designated source folder path and renames it to match the app name. The script also ensures that the output directory exists and deletes any previous DMG files. Finally, it uses the "create-dmg" tool to create the DMG with specified window position, size, icon size, and other options.

---
+++ scripts/create-dmg.sh
@@ -0,0 +1,33 @@
+#!/usr/bin/env bash
+
+APP_VERSION="0.1.0"
+APP_NAME="ChitChat"
+DMG_FILE_NAME="${APP_NAME}-${APP_VERSION}-Installer.dmg"
+VOLUME_NAME="${APP_NAME} v${APP_VERSION} Installer"
+SOURCE_FOLDER_PATH="build/macos/Build/Products/Release"
+DMG_SOURCE_FOLDER_PATH="${SOURCE_FOLDER_PATH}/out"
+OUTPUT_PATH="build/macos/Build/Products/DMG"
+CREATE_DMG=create-dmg
+
+# Make a copy of the chitchat.app, rename it to the "APP_NAME" and then move it to the "DMG_SOURCE_FOLDER_PATH"
+mkdir "${DMG_SOURCE_FOLDER_PATH}" && cp -R "${SOURCE_FOLDER_PATH}/chitchat.app" "${DMG_SOURCE_FOLDER_PATH}/${APP_NAME}.app"
+
+# Make sure the DMG directory exists
+mkdir -p "${OUTPUT_PATH}"
+
+# Since create-dmg does not clobber, be sure to delete previous DMG
+[[ -f "${OUTPUT_PATH}/${DMG_FILE_NAME}" ]] && rm "${OUTPUT_PATH}/${DMG_FILE_NAME}"
+
+# Create the DMG
+$CREATE_DMG \
+  --volname "${VOLUME_NAME}" \
+  --window-pos 200 120 \
+  --window-size 800 400 \
+  --icon-size 100 \
+  --icon "${APP_NAME}.app" 200 190 \
+  --hide-extension "${APP_NAME}.app" \
+  --app-drop-link 600 185 \
+  "${OUTPUT_PATH}/${DMG_FILE_NAME}" \
+  "${DMG_SOURCE_FOLDER_PATH}"
+
+# --background "installer_background.png" \
